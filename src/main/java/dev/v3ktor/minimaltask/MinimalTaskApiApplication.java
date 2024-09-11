cdb4f021fb6e80956521f8acefdf6b3d557f3c95
Bruno C. <v3ktoryt@gmail.com>
2024-09-11 10:12:18 -0300

    first commit

---
+++ src/main/java/dev/v3ktor/minimaltask/MinimalTaskApiApplication.java
@@ -0,0 +1,13 @@
+package dev.v3ktor.minimaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalTaskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimalTaskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/v3ktor/minimaltask/MinimalTaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.v3ktor.minimaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalTaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
