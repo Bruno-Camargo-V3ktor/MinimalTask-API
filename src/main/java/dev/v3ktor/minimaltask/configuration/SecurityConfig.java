dca4c2d74b84c88145ebc677732e539d855d8162
Bruno C <v3ktoryt@gmail.com>
2024-10-27 22:41:23 -0300

    ✨ feat: 'Adicionando um UserDetailsPersonalizado, assim sincronizando a autenticacao com meu banco de dados'

--- src/main/java/dev/v3ktor/minimaltask/configuration/SecurityConfig.java
+++ src/main/java/dev/v3ktor/minimaltask/configuration/SecurityConfig.java
@@ -1,8 +1,9 @@
 package dev.v3ktor.minimaltask.configuration;
 
+import dev.v3ktor.minimaltask.security.MinimalTaskUserDetaisService;
+import dev.v3ktor.minimaltask.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -34,6 +35,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public MinimalTaskUserDetaisService userDetailsService( UserService userService ) {
+        return new MinimalTaskUserDetaisService( userService );
     }
 
 }
---
+++ src/main/java/dev/v3ktor/minimaltask/security/MinimalTaskUserDetaisService.java
@@ -0,0 +1,32 @@
+package dev.v3ktor.minimaltask.security;
+
+import dev.v3ktor.minimaltask.service.UserService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MinimalTaskUserDetaisService implements UserDetailsService {
+
+    // Attributes
+    private final UserService userService;
+
+
+    // Constructor
+    public MinimalTaskUserDetaisService(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Methods
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        var userDb = userService.getUserByUsername(username);
+        
+        return User
+                .withUsername( userDb.getUsername() )
+                .password( userDb.getPassword() )
+                .build();
+    }
+
+}
